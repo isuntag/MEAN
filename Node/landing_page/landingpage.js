baa9eee27eb97342d96f0c460a4f413e957e5155
Isaac <isuntag@gmail.com>
2017-08-02 11:41:56 -0400

    Added dojoquery and fibNacci projects and landing_page node project

---
+++ Node/landing_page/landingpage.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(request, response){
+  console.log('client request URL: ', request.url);
+  if(request.url === "/") {
+    fs.readFile('index.html', 'utf8', function(errors, contents){
+      response.writeHead(200, {'Content-Type':'text/html'});
+      response.write(contents);
+      response.end();
+    });
+  }
+  else if(request.url === "/ninjas") {
+    fs.readFile('ninjas.html', 'utf8', function(errors, contents){
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(contents);
+      response.end();
+    });
+  }
+  else if(request.url === "/dojos/new") {
+    fs.readFile('dojos.html', 'utf8', function(errors, contents){
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(contents);
+      response.end();
+    });
+  }
+  else {
+    response.end('File not found!!!');
+  }
+});
+
+server.listen(6789);
+console.log("Running in localhost at port 6789");
