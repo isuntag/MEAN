7c17de0ed14779c4ed87cbb19655535e33751619
Isaac <isuntag@gmail.com>
2017-08-07 21:20:01 -0400

    Added comments

--- MongoDB/dashboard/server.js
+++ MongoDB/dashboard/server.js
@@ -11,6 +11,7 @@ app.set('views', __dirname + "/views");
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/dashboard');
+// Creates the schema for cats in the database
 var CatSchema = new mongoose.Schema({
     name: {type: String, required: true, minlength: 1},
     age: {type: Number, required: true},
@@ -19,6 +20,7 @@ var CatSchema = new mongoose.Schema({
 mongoose.model('cat', CatSchema);
 var cat = mongoose.model('cat');
 
+// Find all the cats in the database and render the index with their information
 app.get('/', function(req, res) {
     cat.find({}, function(err,cats) {
         if(err){
@@ -28,10 +30,12 @@ app.get('/', function(req, res) {
     });
 });
 
+// Clicking the "Add a Cat" button will redirect to the add a cat form
 app.get('/cats/add', function(req, res) {
     res.render('add');
 });
 
+// Submitted form from /cats/add is routed here and adds a new cat the the database
 app.post('/cats', function(req, res) {
     var add_cat = new cat(req.body);
     add_cat.save(function(err) {
@@ -45,6 +49,7 @@ app.post('/cats', function(req, res) {
     });
 });
 
+// Clicking the "Edit" button will get the information of that specific cat and render it in /cats/:id
 app.get('/cats/:id', function(req, res) {
     cat.find({_id: req.params.id}, function(err, cat) {
         if(err) {
@@ -54,6 +59,7 @@ app.get('/cats/:id', function(req, res) {
     })
 });
 
+// Submitted form data from the edit page will update the information in the databased based on the id of the cat and redirect back to the index
 app.post('/cats/:id', function(req, res) {
     cat.update({_id: req.params.id}, req.body, function(err, result){
         if(err){
@@ -63,7 +69,8 @@ app.post('/cats/:id', function(req, res) {
     });
 });
 
-app.post('/cats/destroy/:id', function(req, res) {
+// Clicking the "Delete" button will remove the cat from the database based on the cat's id and redirect back to index
+app.get('/cats/destroy/:id', function(req, res) {
     cat.remove({_id: req.params.id}, function(err){
         if(err) {
             console.log(err);
