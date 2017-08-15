99e80bb5a3b5acd9ab5ba96b99585500f5a6d025
Isaac <isuntag@gmail.com>
2017-08-15 13:30:35 -0400

    Added comments to github-score

--- angular/github-score/src/app/score/score.component.ts
+++ angular/github-score/src/app/score/score.component.ts
@@ -10,12 +10,16 @@ export class ScoreComponent implements OnInit {
     username="";
     info = [];
     err = "";
+    // make a private variable _taskService that is in the form of the TaskService
   constructor(private _taskService: TaskService) { }
   ngOnInit() {
   }
   onSubmit() {
+    //   on submit send a request to use method retrieveInfo from the TaskService service to get the github information of the entered username
       this._taskService.retrieveInfo(this.username)
+    //   if it returns info then set this.info to the info and reset this.err
       .then(info => {this.info = info; this.err=null})
+    //   if it returns an error then set this.error to err and reset this.info
       .catch(err => {this.err = err; this.info=[]});
   }
 }
--- angular/github-score/src/app/task.service.ts
+++ angular/github-score/src/app/task.service.ts
@@ -5,6 +5,7 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class TaskService {
   constructor(private _http: Http) { }
+  // retrieveInfo will send an api request to the user's github and convert it to a Promise so we can easily use it
   retrieveInfo(name) {
       return this._http.get(`https://api.github.com/users/${name}`).map(data=>data.json()).toPromise();
   }
